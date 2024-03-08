675d5fe1c19f9ce42b394e2e7a40aa71d308fadd
Ragger <67398418+jibzdev@users.noreply.github.com>
2024-03-08 03:16:24 +0000

    workout work

--- main/assets/js/workout.js
+++ main/assets/js/workout.js
@@ -13,59 +13,96 @@ async function fetchWorkoutDetails(id) {
     displayWorkout(workoutDetails[currentWorkoutIndex]);
 
     const PLAY = document.querySelector("#playButton");
-    PLAY.addEventListener('click', async () => {
-        document.querySelector("#screen").innerHTML = '';
-        PLAY.innerHTML = `<i class="fa-solid fa-pause"></i>`;
-        let countdownContainer = document.createElement("div");
-        document.querySelector("#screen").appendChild(countdownContainer);
-        for (let countdown = 5; countdown > 0; countdown--) {
-            await new Promise(resolve => setTimeout(() => {
-                countdownContainer.innerHTML = `Starting in ${countdown}`;
-                resolve();
-            }, 1000));
-        }
-        countdownContainer.remove(); // Remove countdown display
-        
-        while (currentWorkoutIndex < workoutDetails.length) {
-            let workout = workoutDetails[currentWorkoutIndex];
-            let workoutNameForGif = workout.Workout_Name.toLowerCase().replace(/\s+/g, '').replace(/-/g, '');
-            let workoutContainer = document.createElement("div");
-            workoutContainer.innerHTML = `
-            <img src="/assets/img/gifs/${workoutNameForGif}.gif" alt="Workout In Progress">
-            <p>${workout.Workout_Description}</p>
-            <p id="workoutTimer">10 seconds</p>
-            `;
-            document.querySelector("#screen").append(workoutContainer);
-            let workoutDuration = 2;
-            for (let seconds = workoutDuration; seconds > 0; seconds--) {
-                await new Promise(resolve => setTimeout(() => {
-                    document.getElementById("workoutTimer").innerHTML = `${seconds} seconds`;
-                    resolve();
-                }, 1000));
-            }
-            workoutContainer.remove();
-            notify("Good work, keep going!","green")
-            currentWorkoutIndex++;
-            if (currentWorkoutIndex < workoutDetails.length) {
-                displayWorkout(workoutDetails[currentWorkoutIndex]);
-            }
-        }
-        
-        if (currentWorkoutIndex >= workoutDetails.length) {
-            notify("Plan Completed!","green")
-            document.querySelector("#upNext").innerHTML = "All workouts completed!";
+    const BACK = document.querySelector("#backButton");
+    const FORWARD = document.querySelector("#forwardButton");
+    let isPlaying = false;
+    let workoutInterval;
+
+    PLAY.addEventListener('click', () => {
+        document.querySelector("#screen").classList.add("fade");
+        if (isPlaying) {
+            clearInterval(workoutInterval);
             PLAY.innerHTML = `<i class="fa-solid fa-play"></i>`;
+        } else {
+            if (currentWorkoutIndex >= workoutDetails.length) {
+                currentWorkoutIndex = 0;
+            }
+            startWorkout();
+        }
+        isPlaying = !isPlaying;
+    });
+
+    BACK.addEventListener('click', () => {
+        document.querySelector("#screen").classList.add("fade");
+        if (currentWorkoutIndex > 0) {
+            clearInterval(workoutInterval);
+            currentWorkoutIndex--;
+            startWorkout();
         }
     });
+
+    FORWARD.addEventListener('click', () => {
+        document.querySelector("#screen").classList.add("fade");
+        if (currentWorkoutIndex < workoutDetails.length - 1) {
+            clearInterval(workoutInterval);
+            currentWorkoutIndex++;
+            startWorkout();
+        }
+    });
+
+    async function startWorkout() {
+        document.querySelector("#screen").innerHTML = '';
+        document.querySelector("#screen").classList.add("fade");
+        PLAY.innerHTML = `<i class="fa-solid fa-pause"></i>`;
+        displayWorkout(workoutDetails[currentWorkoutIndex + 1]);
+        let workout = workoutDetails[currentWorkoutIndex];
+        let workoutNameForGif = workout.Workout_Name.toLowerCase().replace(/\s+/g, '').replace(/-/g, '');
+        let workoutContainer = document.createElement("div");
+        let workoutDurationInSeconds = parseInt(workout.Workout_Duration, 10) * 60;
+        workoutContainer.innerHTML = `
+        <p>Current Workout:<br>${workout.Workout_Name}</p><br>
+        <img src="/assets/img/gifs/${workoutNameForGif}.gif" alt="Workout In Progress"><br>
+        <p>${workout.Workout_Description}</p>
+        <p id="workoutTimer">${Math.floor(workoutDurationInSeconds / 60)}:${(workoutDurationInSeconds % 60).toString().padStart(2, '0')}</p>
+        `;
+        document.querySelector("#screen").append(workoutContainer);
+        let remainingSeconds = workoutDurationInSeconds;
+        workoutInterval = setInterval(async () => {
+            if (remainingSeconds <= 0) {
+                clearInterval(workoutInterval);
+                workoutContainer.remove();
+                notify("Keep Going!", "green");
+                currentWorkoutIndex++;
+                if (currentWorkoutIndex < workoutDetails.length - 1) {
+                    startWorkout();
+                } else if (currentWorkoutIndex === workoutDetails.length - 1) {
+                    displayWorkout(workoutDetails[currentWorkoutIndex]);
+                    notify("Final Workout!", "green");
+                } else {
+                    notify("Plan Completed!", "green");
+                    document.querySelector("#screen").innerHTML = "<h1>Workout Plan Completed!</h1><p>Press play to replay.</p>";
+                    PLAY.innerHTML = `<i class="fa-solid fa-play"></i>`;
+                    isPlaying = false;
+                }
+            } else {
+                let minutes = Math.floor(remainingSeconds / 60);
+                let seconds = remainingSeconds % 60;
+                document.getElementById("workoutTimer").innerHTML = `${minutes}:${seconds.toString().padStart(2, '0')}`;
+                remainingSeconds--;
+            }
+        }, 1000);
+    }
 }
 
 function displayWorkout(workout) {
     const workoutInfo = `
-    <div>
-        <h3>Up Next: ${workout.Workout_Name}</h3>
-        <p>${workout.Workout_Description}</p>
-    </div>
+    <img src="assets/img/workoutImgs/${workout.Workout_ID}.jpg">
+    <div id="textNext">
+        <h1>Up Next</h1>
+        <p>${workout.Workout_Name}</p>
+    </div>    
     `;
+    document.querySelector("#upNext").classList = "fade";
     document.querySelector("#upNext").innerHTML = workoutInfo;
 }
 
@@ -88,6 +125,7 @@ document.addEventListener('DOMContentLoaded', async function() {
             const totalDuration = workoutDetails.reduce((acc, workout) => acc + parseInt(workout.Workout_Duration, 10), 0);
             document.querySelector("#screen").innerHTML = `Total Workout Duration: ${totalDuration} minutes<br>Press play to begin`;
             fetchWorkoutDetails(id);
+            showAllWorkoutsButton(workoutDetails);
         } catch (error) {
             console.error(error.message);
         }
@@ -95,3 +133,44 @@ document.addEventListener('DOMContentLoaded', async function() {
         console.error('No PlanID provided in the query parameters.');
     }
 });
+
+function showAllWorkoutsButton(workoutDetails){
+    document.querySelector("#showAllWorkouts").addEventListener("click", function() {
+        let overlay = document.createElement("div");
+        overlay.classList = "fade";
+        overlay.id = "overlay";
+        overlay.style.position = "fixed";
+        overlay.style.top = "0";
+        overlay.style.left = "0";
+        overlay.style.width = "100%";
+        overlay.style.height = "100%";
+        overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+        overlay.style.zIndex = "1000";
+        
+        let allWorkoutsDiv = document.createElement("div");
+        allWorkoutsDiv.id = "allWorkoutsContainer";
+        
+        let closeButton = document.createElement("button");
+        closeButton.innerText = "X";
+        closeButton.style.position = "fixed";
+        closeButton.style.top = "5px";
+        closeButton.style.right = "10px";
+        closeButton.onclick = function() {
+            overlay.remove();
+        };
+        
+        allWorkoutsDiv.appendChild(closeButton);
+        workoutDetails.forEach(workout => {
+            const workoutElement = document.createElement("div");
+            workoutElement.id = "workoutElementCard";
+            workoutElement.innerHTML = `
+            <img src="assets/img/workoutImgs/${workout.Workout_ID}.jpg">
+            <h3>${workout.Workout_Name}</h3>
+            <p>${workout.Workout_Duration} Minutes</p>`;
+            allWorkoutsDiv.appendChild(workoutElement);
+        });
+        
+        overlay.appendChild(allWorkoutsDiv);
+        document.body.append(overlay);
+    });
+}
